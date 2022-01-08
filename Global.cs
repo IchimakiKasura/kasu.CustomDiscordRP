0207adcccedeb4ffd11939abe0c8e22620d8060d
Ichimaki <ichimakiKasura@gmail.com>
2022-01-08 11:46:52 +0800

    Added New Files

---
+++ Global.cs
@@ -0,0 +1,14 @@
+﻿global using System;
+global using System.Linq;
+global using System.Text;
+global using System.Windows;
+global using System.Windows.Data;
+global using System.Windows.Input;
+global using System.Windows.Media;
+global using System.Windows.Shapes;
+global using System.Threading.Tasks;
+global using System.Windows.Controls;
+global using System.Windows.Documents;
+global using System.Windows.Navigation;
+global using System.Collections.Generic;
+global using System.Windows.Media.Imaging;
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,26 @@
+﻿namespace kasu.CustomDiscordRP
+{
+	/// <summary>
+	/// Interaction logic for MainWindow.xaml
+	/// </summary>
+	public partial class MainWindow : Window
+	{
+		public MainWindow()
+		{
+			InitializeComponent();
+		}
+
+		#region Top buttons
+		private void TopButtons(object s, CanExecuteRoutedEventArgs e) { e.CanExecute = true; }
+		private void CloseCommand(object s, ExecutedRoutedEventArgs e) { SystemCommands.CloseWindow(this); }
+		private void MinimizeCommand(object s, ExecutedRoutedEventArgs e) { SystemCommands.MinimizeWindow(this); }
+
+		#endregion
+
+		private void Window_Drag(object sender, MouseButtonEventArgs e)
+		{
+			if (e.ChangedButton == MouseButton.Left)
+				this.DragMove();
+		}
+	}
+}
