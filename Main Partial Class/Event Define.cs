7045f4b70fad329bfa714270e9231c433aea96b2
Ichimaki <ichimakiKasura@gmail.com>
2022-02-13 18:59:23 +0800

    Dev Commit
    [Added Config]
    - You can modify the application's few things* on "Config.kasu"

---
+++ Main Partial Class/Event Define.cs
@@ -0,0 +1,50 @@
+﻿namespace kasu.CustomDiscordRP;
+
+public partial class MainWindow
+{
+	public static MainWindow MW;
+	public kasu_Discord client;
+	private bool IsDebug = false;
+
+	private void InitializeListeners()
+	{
+
+		MW = this;
+
+		#region Top Buttons
+
+			#region close
+			CloseButton.MouseEnter += kMouseEnter;
+			CloseButton.MouseLeave += kMouseLeave;
+			#endregion
+
+			#region Minimize
+			MinimizeButton.MouseEnter += kMouseEnter;
+			MinimizeButton.MouseLeave += kMouseLeave;
+		#endregion
+
+		#endregion
+
+		#region Inputs
+
+			#region Inputs Events
+			Input_Details.TextChanged += DetailOnChange;
+			Input_State.TextChanged += StateOnChange;
+			Input_ButtonOneText.TextChanged += ButtonPreview;
+			Input_ButtonTwoText.TextChanged += ButtonPreview;
+			Input_Time.TextChanged += NumberBoxOnChange;
+			Input_PartySizeMin.TextChanged += NumberBoxOnChange;
+			Input_PartySizeMax.TextChanged += NumberBoxOnChange;
+			#endregion
+
+			#region Inputs Attributes
+			Input_ButtonOneText.MaxLength =
+			Input_ButtonTwoText.MaxLength = 20;
+
+			Button_Connect.Click += ConnectClick;
+			Button_Disconnect.Click += DisconnectClick;
+			#endregion
+
+		#endregion
+	}
+}
--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -2,10 +2,6 @@
 {
 	public partial class MainWindow : Window
 	{
-		public static MainWindow MW;
-		public kasu_Discord client;
-		private bool IsDebug = false;
-
 		public MainWindow()
 		{
 			InitializeComponent();
@@ -16,46 +12,11 @@
 			// Loads the Custom settings on the "Customize_Preview.txt"
 			LoadCustomPreview();
 
-			MW = this;
-
-			#region Top Buttons
-			
-				#region close
-				CloseButton.MouseEnter += kMouseEnter;
-				CloseButton.MouseLeave += kMouseLeave;
-				#endregion
-				
-				#region Minimize
-				MinimizeButton.MouseEnter += kMouseEnter;
-				MinimizeButton.MouseLeave += kMouseLeave;
-				#endregion
-				
-			#endregion
+			// Setups all the inputs/buttons event args.
+			InitializeListeners();
 
 			// status bar
 			Statusbar.Foreground = DisconnectColor;
-
-			#region Inputs
-
-				#region Inputs Events
-				Input_Details.TextChanged += DetailOnChange;
-				Input_State.TextChanged += StateOnChange;
-				Input_ButtonOneText.TextChanged += ButtonPreview;
-				Input_ButtonTwoText.TextChanged += ButtonPreview;
-				Input_Time.TextChanged += NumberBoxOnChange;
-				Input_PartySizeMin.TextChanged += NumberBoxOnChange;
-				Input_PartySizeMax.TextChanged += NumberBoxOnChange;
-				#endregion
-				
-				#region Inputs Attributes
-				Input_ButtonOneText.MaxLength =
-				Input_ButtonTwoText.MaxLength = 20;
-
-				Button_Connect.Click += ConnectClick;
-				Button_Disconnect.Click += DisconnectClick;
-				#endregion
-				
-			#endregion
 		
 		}
 	}
